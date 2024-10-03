017c10ffd8687b2e8b9ddbff42e6a465c138b91b
Максим Хаметов ПКС-305 <khametov04@list.ru>
2024-10-03 11:19:55 +0300

    Добавьте файлы проекта.

---
+++ Freedom.AppHost/Program.cs
@@ -0,0 +1,11 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.Freedom_ApiService>("Api");
+
+builder.AddProject<Projects.Freedom_Web>("WebClient")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService);
+
+builder.AddProject<Projects.Freedom_WebAdministrator>("WebAdmin");
+
+builder.Build().Run();
